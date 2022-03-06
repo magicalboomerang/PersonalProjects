53a742de169a953866098a1f83189296eaee3bbe
magicalboomerang <tim.feusse@gmail.com>
2022-03-05 19:25:51 -0500

    Added library construction plan.

---
+++ C/Libraries/ScreenString.c
@@ -0,0 +1,24 @@
+#ifndef SCREENSTRING
+#define SCREENSTRING
+
+#Plan of execution:
+#    1. 2d memory operations, and basic structs.
+#    2. Image base struct.
+#    3. Image creation functions
+#    4. Layer stuct + functions
+#    5. Base layer struct (zeroth layer)
+#    6. Screen struct + functions
+#    7. Display Objects
+
+// Structs
+struct iPoint_2D{
+	int X;
+	int Y;
+}
+
+
+// Functions
+
+
+
+#endif
